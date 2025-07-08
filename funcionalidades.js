4580f3eaa3d757178addc965a343d8164bd0e4ae
Gabriel Eduardo <gabriel_billynelson@outlook.com>
2025-07-08 15:26:14 -0300

    adicao de estilizacoes no site
    imagem de fundo, imagem de fundos nas tarefas, adicao de fontes

--- funcionalidades.js
+++ funcionalidades.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
             tarefa.className = "tarefa"
             tarefa.innerHTML = `
                 <strong class="tituloTarefa">${titulo}</strong>
-                <p class="descricaoTarefa">${descricao}</p>
+                <textarea class="descricaoTarefa" rows="6" cols="25" readonly >${descricao}</textarea>
                 <button class="botaoConcluir"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
                                                         <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
                                                   </svg><p>Concluir</p></button>
